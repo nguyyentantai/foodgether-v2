5c3d9a6255278e321c524e6132acb1db093e6c9d
Lam Nguyen <nguyenquanglam3008@gmail.com>
2022-08-17 00:08:10 +0700

    Debug log

--- core/pages/api/restaurants/[id].ts
+++ core/pages/api/restaurants/[id].ts
@@ -8,6 +8,7 @@ const handler: NextApiHandler = async (req, res) => {
       return res.status(200).json(result)
     }
   } catch (err) {
+    console.log(err)
     return res.status(500).json({ err })
   }
 }
